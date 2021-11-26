ed16b1ecd58f69868cda7d9b1db4180afa7c43e0
Guilherme Ferreira <GuiFerreira01>
2021-11-25 23:50:01 -0300

    Voltando

--- src/Produtos.js
+++ src/Produtos.js
@@ -3,4 +3,15 @@ import Navbar from './Pages/Navbar'
 import Footer from './Pages/Footer';
 
 
+function Produto () {
+        return(
+            <div>
+                <Navbar />
+                <Pord />
+                <Footer />
+            </div>
+        )
+    }
+
+
     export default Produto;
