7dc69c84c89b825a2015a2d3ab9154a4b143cc6f
Guilherme Ferreira <GuiFerreira01>
2021-11-25 23:42:17 -0300

    Teste

--- src/Produtos.js
+++ src/Produtos.js
@@ -3,15 +3,4 @@ import Navbar from './Pages/Navbar'
 import Footer from './Pages/Footer';
 
 
-function Produto () {
-        return(
-            <div>
-                <Navbar />
-                <Pord />
-                <Footer />
-            </div>
-        )
-    }
-
-
     export default Produto;
